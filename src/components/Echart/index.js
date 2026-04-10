582c45a4092ec538846982958cdabab4c5d2d2ee
Linzp <sunandmoon001@163.com>
2026-04-10 16:59:26 +0800

    修改echart bug

--- src/components/Echart/index.js
+++ src/components/Echart/index.js
@@ -12,19 +12,36 @@ const loader = {
 };
 
 const EchartInner = ({ data, option, ...props }) => {
+  const optionRef = useRef(option);
+  optionRef.current = option;
   const ref = useResize(() => {
-    instanceRef.current && instanceRef.current.resize();
+    if (instanceRef.current) {
+      instanceRef.current.resize();
+      if (ref.current && (ref.current.clientWidth > 0 && ref.current.clientHeight > 0) && optionRef.current) {
+        instanceRef.current.setOption(optionRef.current);
+      }
+    }
   });
   const instanceRef = useRef(null);
   const { echarts } = data;
   useEffect(() => {
-    if (!echarts) {
+    if (!echarts || !ref.current) {
       return;
     }
     instanceRef.current = echarts.init(ref.current);
-  }, [echarts, ref]);
+    return () => {
+      instanceRef.current && instanceRef.current.dispose();
+      instanceRef.current = null;
+    };
+  }, [echarts]);
   useEffect(() => {
-    instanceRef.current && instanceRef.current.setOption(option);
+    if (!instanceRef.current) {
+      return;
+    }
+    if (ref.current && (ref.current.clientWidth === 0 || ref.current.clientHeight === 0)) {
+      return;
+    }
+    instanceRef.current.setOption(option);
   }, [option]);
   return <div {...props} ref={ref} />;
 };
